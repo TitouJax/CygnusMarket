96df267fb95fe6a6d3a60957caa8eca408cd7f84
TitouJax <tristan.gadaud@gmail.com>
2021-03-19 17:21:05 +0200

    Adding landing page

--- src/main/java/shop/client/ClientApplication.java
+++ src/main/java/shop/client/ClientApplication.java
@@ -2,16 +2,14 @@ package shop.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @SpringBootApplication
 @RestController
-class Application {
+class Application{
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-	@GetMapping(value = "/")
-	public String hello(){
-		return "hello";
 	}
 }
